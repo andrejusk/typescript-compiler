979b8606fa674e7bb900b0f82ce2277fce09469e
Andrejus <andrejus@think-engineer.com>
2018-02-19 00:35:15 +0000

    Add reserved tokens to lexical analyser

---
+++ src/lexical-analyser.ts
@@ -0,0 +1,29 @@
+/* 
+    Tokens 
+*/
+
+/* Punctuation */
+let COMMA = ','
+let COLON = ':'
+let SEIMCOLON = ';'
+let DOT = '.'
+
+/* Arithmetic */
+let EQUALS = '='
+let PLUS = '+'
+
+/* Brackets */
+let OPEN_PARENTHESIS = '('
+let CLOSE_PARENTHESIS = ')'
+let OPEN_CURLY_BRACKET = '{'
+let CLOSE_CURLY_BRACKET = '}'
+let OPEN_ANGLE_BRACKET = '<'
+let CLOSE_ANGLE_BRACKET = '>'
+let OPEN_SQUARE_BRACKET = '['
+let CLOSE_SQUARE_BRACKET = ']'
+
+/* Keywords */
+let LET = 'let'
+
+/* Variable objects */
+let NUMBER = 'number'
