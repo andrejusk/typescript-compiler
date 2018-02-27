8a85a8bcafaa8affb8969f58b8b837776292a1f3
Andrejus <andrejus@think-engineer.com>
2018-02-27 22:49:41 +0000

    Clean up lexical analyser

--- src/lexical-analyser.ts
+++ src/lexical-analyser.ts
@@ -8,8 +8,8 @@ enum Type {
     'RESERVED'
 }
 
-/** Abbereviated lexim types */
-enum Type_abbr {
+/** Abbereviated lexeme types */
+enum TypeS {
     'WHITE',
     'PUNCT',
     'IDENT',
@@ -18,18 +18,13 @@ enum Type_abbr {
 }
 
 
-/** Token class, value and lexim optional */
+
+/** Token class, value and lexeme optional */
 class Token {
     type: Type
     name: string
-    lexim: string
+    lexeme: string
     value: number
-    v_location: number
-    h_location: number
-}
-
-/** Position class, holds vertical and horizontal character values */
-class Position {
     v_location: number
     h_location: number
 }
@@ -38,23 +33,21 @@ class Position {
  * Creates and returns Token
  * @param type Type of Token
  * @param name Name of Token
- * @param lexim Lexim the Token was parsed from
+ * @param lexeme Lexim the Token was parsed from
  * @param location Location in source file
  */
-function createToken(type: Type, name: string, lexim: string): Token {
-
-    let location = getCurrentCharacterIndex()
+function createToken(type: Type, name: string, lexeme: string): Token {
+    let location: number = getCurrentCharacterIndex()
 
     let position: Position = getLocation(location)
-
     let v_location: number = position.v_location
     let h_location: number = position.h_location
 
-    let value = undefined
+    let value: number = undefined
 
     if (type == Type.CONSTANT) {
-        value = Number(lexim)
-        lexim = null
+        value = Number(lexeme)
+        lexeme = null
     } else {
         value = null
     }
@@ -62,13 +55,42 @@ function createToken(type: Type, name: string, lexim: string): Token {
     return {
         'type':         type,
         'name':         name,
-        'lexim':        lexim,
+        'lexeme':       lexeme,
         'value':        value,
         'v_location':   v_location,
         'h_location':   h_location,
     }
-
 }
+
+
+/** Position class, holds vertical and horizontal character values */
+class Position {
+    v_location: number
+    h_location: number
+}
+
+/**
+ * Returns line number
+ * @param position 
+ */
+function getLocation(position: number): Position {
+    let lines: number = 1
+    let chars: number = 1
+    let iterator: number = 0
+
+    while (iterator < position) {
+        if (getCharacter(iterator) == WHITESPACE.NEWLINE) {
+            chars = 1
+            lines++
+        }
+        chars++
+        iterator++
+    }
+
+    return { 'h_location': chars, 'v_location': lines }
+}
+
+
 
 /** Symbol to name mapping */
 interface SymbolMap {
@@ -89,27 +111,27 @@ const WHITESPACE: SymbolMap = {
 
 /** PUNCT - Punctuation */
 const PUNCTUATION: SymbolMap = {
-    'COMMA':                    ',',
-    'COLON':                    ':',
-    'SEIMCOLON':                ';',
-    'DOT':                      '.',
-    'SINGLE_QUOTE':             '\'',
-    'DOUBLE_QUOTE':             '.',
-    'BACKSLASH':                '\\',
-    'FWDSLASH':             '/',
-    'ASTERISK':                 '*',
-    'EXCLAMATION':              '!',
-    'AMPERSAND':                '&',
-    'EQUALS':                   '=',
-    'PLUS':                     '+',
-    'MINUS':                    '-',  
-    'O_PAR':         '(',
+    'COMMA':        ',',
+    'COLON':        ':',
+    'SEIMCOLON':    ';',
+    'DOT':          '.',
+    'SINGLE_QUOTE': '\'',
+    'DOUBLE_QUOTE': '.',
+    'BACKSLASH':    '\\',
+    'FWDSLASH':     '/',
+    'ASTERISK':     '*',
+    'EXCLAMATION':  '!',
+    'AMPERSAND':    '&',
+    'EQUALS':       '=',
+    'PLUS':         '+',
+    'MINUS':        '-',  
+    'O_PAR':        '(',
     'C_PAR':        ')',
-    'O_CUR_BR':       '{',
-    'C_CUR_BR':      '}',
-    'O_ANG_BR':       '<',
-    'C_ANG_BR':      '>',
-    'O_SQR_BR':      '[',
+    'O_CUR_BR':     '{',
+    'C_CUR_BR':     '}',
+    'O_ANG_BR':     '<',
+    'C_ANG_BR':     '>',
+    'O_SQR_BR':     '[',
     'C_SQR_BR':     ']'
 }
 
@@ -152,13 +174,15 @@ function getKey(map: SymbolMap, term: string): string {
 
 
 
+
+
 import fs = require('fs')
 import colors = require('colors/safe')
 import { getMaxListeners } from 'cluster'
 
-let debugFlag
+let debugFlag: boolean
 
-let content
+let content: string[]
 let currentCharacterIndex: number = 0
 
 /**
@@ -166,17 +190,16 @@ let currentCharacterIndex: number = 0
  * @param filePath File to parse.
  * @param debug Print debug messages.
  */
-export function parseFile(filePath, debug: boolean = false) {
+export function parseFile(filePath: string, debug: boolean = false): Token[] {
     debugFlag = debug
 
-    let rawContent = fs.readFileSync(filePath, 'utf8')
-    content = rawContent.split('')
+    content = fs.readFileSync(filePath, 'utf8').split('')
 
-    let tokens = new Array()
+    let tokens: Token[] = new Array()
     
     while (true) {
         /* Parse character */
-        tokens.push(parseCharacter())
+        tokens.push(parse())
         /* EOF */
         if (currentCharacterIndex >= content.length) {
             break
@@ -186,46 +209,21 @@ export function parseFile(filePath, debug: boolean = false) {
     return tokens
 }
 
-/**
- * Returns line number
- * @param position 
- */
-function getLocation(position: number): Position {
-    let lines: number = 1
-    let chars: number = 1
-    let iterator: number = 0
-
-    while (iterator < position) {
-        if (getCharacter(iterator) == WHITESPACE.NEWLINE) {
-            chars = 1
-            lines++
-        }
-        chars++
-        iterator++
-    }
-
-    return { 'h_location': chars, 'v_location': lines }
-}
 
 
 
-/** Increments current character index and parses character. */
-function parseNextCharacter(): Token {
-    nextCharacter()
-    return parseCharacter()
-}
 
 /** 
- * Parses character and returns its lexical token. 
+ * Parses character and returns its lexical Token. 
  * 
  * @returns
  */
-function parseCharacter(): Token {
+function parse(): Token {
 
     /* Skip line comments */
     if (getCurrentCharacter() == PUNCTUATION['FWDSLASH'] && peekNextCharacter() == PUNCTUATION['FWDSLASH']) {
         skipUntil(WHITESPACE['NEWLINE'])
-        return parseNextCharacter()
+        return parseNext()
     }
 
     /* Skip block comments */
@@ -235,43 +233,43 @@ function parseCharacter(): Token {
             skipUntil(PUNCTUATION['ASTERISK'])
         }
         nextCharacter()
-        return parseNextCharacter()
+        return parseNext()
     }
 
     /* Skip whitespace */
     if (isMapped(WHITESPACE, getCurrentCharacter())) {
-        return parseNextCharacter()
+        return parseNext()
     }
 
-    let token: Token
+    let Token: Token
 
     /* Parse punctuation */
     let tempKey = getKey(PUNCTUATION, getCurrentCharacter())
     if (tempKey != null) {
-        token = createToken(Type.PUNCTUATION, tempKey, PUNCTUATION[tempKey])
+        Token = createToken(Type.PUNCTUATION, tempKey, PUNCTUATION[tempKey])
         nextCharacter()
     }
 
     /* Parse words */
     else {
-        token = parseNextWord()
+        Token = parseNextWord()
     }
 
     if (debugFlag) {
-        log(token)
+        log(Token)
     }
 
-    return token
+    return Token
 
 }
 
-
-/* 
- * Helper functions 
- */
+/** Increments current character index and parses character. */
+function parseNext(): Token {
+    nextCharacter()
+    return parse()
+}
 
 /** 
- * 
  * 
  * @returns
  */
@@ -285,13 +283,18 @@ function parseNextWord(): Token {
 
     while(!isMapped(WHITESPACE, tempCharacter) && !isMapped(PUNCTUATION, tempCharacter)) {
         word += tempCharacter
-        //todo: check EOF
+        
+        /* EOF */
+        if (tempIndex >= content.length) {
+            break
+        }
+
         tempCharacter = getCharacter(++tempIndex)
     }
 
     setCurrentCharacterIndex(--tempIndex)
 
-    let key = getKey(RESERVED, word)
+    let key: string = getKey(RESERVED, word)
 
     /* Reserved word */
     if (key != null) {   
@@ -311,24 +314,44 @@ function parseNextWord(): Token {
     nextCharacter()
 
     return token
-
 }
 
-/** Increments current index. */
-function nextCharacter() {
-    currentCharacterIndex++
-}
+
+
+
+
+/* 
+ * Helper functions 
+ */
 
 /** Returns current index. */
 function getCurrentCharacterIndex(): number {
     return currentCharacterIndex
 }
 
-function setCurrentCharacterIndex(index: number) {
+/** Sets current character in file. */
+function setCurrentCharacterIndex(index: number): void {
     currentCharacterIndex = index
 }
 
-/** Returns current character in file. */
+/** Increments current index. */
+function nextCharacter(): void {
+    return setCurrentCharacterIndex(getCurrentCharacterIndex() + 1)
+}
+
+/**
+ * Moves index to next occurance of target.
+ * @param target Character to look for.
+ */
+function skipUntil(target: string): void {
+    while (getCurrentCharacter() != target) {
+        return nextCharacter()
+    }
+}
+
+
+
+/** Returns given character in file. */
 function getCharacter(characterIndex: number): string {
     return content[characterIndex]
 }
@@ -343,24 +366,15 @@ function peekNextCharacter(): string {
     return getCharacter(currentCharacterIndex + 1)
 }
 
-/**
- * Moves index to next occurance of target.
- * @param target Character to look for.
- */
-function skipUntil(target: string) {
-    while (getCurrentCharacter() != target) {
-        nextCharacter()
-    }
-}
+
 
 
 
 /** Debug print function */ 
-function log(token: Token) {
+function log(Token: Token): void {
+    let nameColor: colors
 
-    let nameColor
-
-    switch (token.type) {
+    switch (Token.type) {
         case Type.CONSTANT:
             nameColor = colors.green
             break
@@ -383,13 +397,12 @@ function log(token: Token) {
     console.log(
         colors.blue(`  >>`) +
         colors.grey(`\t[`) +
-        nameColor(`${Type_abbr[token.type]}`) +
+        nameColor(`${TypeS[Token.type]}`) +
         colors.grey(`] `) +
-        nameColor(`${token.name}\t`) +
+        nameColor(`${Token.name}\t`) +
         colors.grey(`at `) +
-        colors.yellow(`${token.v_location}`) +
+        colors.yellow(`${Token.v_location}`) +
         `:` +
-        colors.yellow(`${token.h_location}`)
+        colors.yellow(`${Token.h_location}`)
     )
-
 }
