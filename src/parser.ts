f5aa831b85ac5956d2b5320bae574ba6ff0414ef
Andrejus <andrejus@think-engineer.com>
2018-03-06 01:04:43 +0000

    Seperate variable and constant creation in log parser

--- src/parser.ts
+++ src/parser.ts
@@ -209,14 +209,21 @@ function parsePrint(): SyntaxTree {
 
     let action: Token = getToken(currentIndex + 2)
     let identifier: Token = getToken(currentIndex + 4)
-    currentIndex += 6
-    return {
-        content: action,
-        argument1: {
+    let value: SyntaxTree    
+
+    if (identifier.name == 'string') {
+        value = createConstant(identifier)
+    } else {
+        value = {
             content: VARIABLE,
             argument1: { content: identifier },
             argument2: { content: getType(identifier) }
         }
+    }
+    currentIndex += 6
+    return {
+        content: action,
+        argument1: value
     }
 
 }
